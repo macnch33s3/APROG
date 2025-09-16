e4ff123d6b1d08d3b30bb472bf14c40a80d1b89f
macnch33s3 <114980592+macnch33s3@users.noreply.github.com>
2025-09-16 15:54:03 +0200

    added ascii gen

--- ASCII_gen.py
+++ ASCII_gen.py
@@ -3,8 +3,17 @@
 # convert to ASCII
 # Output to console
 
-## text input
-text = input("Enter your text: ")
-#print(text)
+## choose rendering style and convert to ASCII
+from pyfiglet import Figlet
 
-## choose rendering style
+## text input
+user_text = input("Enter your text: ")
+user_font = input("Enter font: ")
+
+# print(user_text)
+
+# chose font
+f = Figlet(font=user_font)
+
+# output to console
+print(f.renderText(user_text))
