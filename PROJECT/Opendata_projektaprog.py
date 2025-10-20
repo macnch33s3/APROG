51d1c08eb8e259e24a99cf69c3c388d9c9930920
macnch33s3 <114980592+macnch33s3@users.noreply.github.com>
2025-10-20 16:00:58 +0200

    small change to opendata

--- PROJECT/Opendata_projektaprog.py
+++ PROJECT/Opendata_projektaprog.py
@@ -3,15 +3,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from pathlib import Path
 
-csv_path = Path(r"C:\\Users\\marck\\Documents\\_OST\\APROG\\open_data_sg\\ladestationen-fur-elektroautos-im-kanton-stgallen.csv")
+# csv_path = Path("C:\\Users\\marck\\OneDrive - OST\\OST RJ\\WING\\WING - 5 Semester\\APROG\\APROG\\PROJECT\\ladestationen-fur-elektroautos-im-kanton-stgallen.csv")
+df = pd.read_csv('C:\Users\marck\Documents\_OST\APROG\open_datasg\ladestationen-fur-elektroautos-im-kanton-stgallen.csv')
+print(df)
 
+#print("current working dir:", Path.cwd())
+#print("CSV path:", csv_path)
+#print("Exists:", csv_path.exists())
 
-print("current working dir:", Path.cwd())
-print("CSV path:", csv_path)
-print("Exists:", csv_path.exists())
-
-if not csv_path.exists():
-    raise FileNotFoundError(f"CSV not found!")
-
-df = pd.read_csv(csv_path)
-print(df.head(3))
+#if not csv_path.exists():
+#    raise FileNotFoundError(f"CSV not found!")
+#
+#df = pd.read_csv(csv_path)
+#print(df.head(3))
